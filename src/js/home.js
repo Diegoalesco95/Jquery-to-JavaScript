c3c1bfa667106ceeba29e9fb286f7a9d965cd337
Diego Alejandro Osorio <42728227+Diegoalesco95@users.noreply.github.com>
2020-02-20 14:13:58 -0500

    Funciones Asíncronas

--- src/js/home.js
+++ src/js/home.js
@@ -22,3 +22,29 @@ fetch("https://randomuser.me/api/") // Retorna una promesa
   .catch(function() {
     console.log("Algo Falló");
   });
+
+// Funciones Asíncronas
+
+(async function load() {
+  // await
+  // action
+  // drama
+  // animation
+  async function getData(url) {
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+  }
+  const actionList = await getData(
+    "https://yts.mx/api/v2/list_movies.json?genre=action"
+  );
+  const dramaList = await getData(
+    "https://yts.mx/api/v2/list_movies.json?genre=drama"
+  );
+  const animationList = await getData(
+    "https://yts.mx/api/v2/list_movies.json?genre=animation"
+  );
+  console.log(actionList);
+  console.log(dramaList);
+  console.log(animationList);
+})();
